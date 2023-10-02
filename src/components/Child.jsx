21812509dbbd651881360dedd4d8f223c0196b36
Khan Imran <erkhanimrancs@gmail.com>
2023-10-03 00:08:48 +0530

    2nd

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 
-const Child = ({ idx, item, price, removeItemHandler }) => {
-	return (
-		<li key={`${idx}-${price}`}>
-			{item} - ${price}
-			<button onClick={() => removeItemHandler(idx)}>Remove</button>
-		</li>
-	);
-};
+const Child = ({ itemList, setItemList }) =>{
+
+    function handleClick(key){
+        setItemList(itemList => 
+            itemList.filter(item => item.key !== key)
+        );
+    }
+
+    return (
+        <div className="child">
+            <h2>Child Component</h2>
+            {
+                itemList.map(item =>
+                    <li key={item.key}>
+                        {item.itemName} - ${item.itemPrice} 
+                        <button onClick={()=>handleClick(item.key)}>Remove</button> 
+                    </li>
+                )
+            }
+        </div>
+    )
+}
 
 export default Child;
--- src/components/Parent.jsx
+++ src/components/Parent.jsx
@@ -2,55 +2,32 @@ import React, { useState } from "react";
 import Child from "./Child";
 
 const Parent = () => {
-	const [itemName, setItemName] = useState("");
-	const [itemPrice, setItemPrice] = useState(null);
-	const [itemList, setItemList] = useState([]);
+    const [itemName, setItemName] = useState('');
+    const [itemPrice, setItemPrice] = useState('');
+    const [itemList, setItemList] = useState([
+        {key: 0, itemName: 'item1', itemPrice: 2},
+        {key: 1, itemName: 'item2', itemPrice: 3},
+        {key: 2, itemName: 'item3', itemPrice: 4},
+    ]);
+    const [key, setKey] = useState(itemList.length);
+    const handleClick = () => {
+        setItemList([...itemList, {key: key, itemName: itemName, itemPrice: itemPrice}]);
+        setKey(key+1);
+        setItemName('');
+        setItemPrice('');
+    }
 
-	const addItemHandler = () => {
-		setItemList([...itemList, { itemName, itemPrice }]);
-	};
-	const removeItemHandler = (idx) => {
-		const firstPart = itemList.slice(0, idx);
-		const lastPart = itemList.slice(idx + 1);
-
-		setItemList([...firstPart, ...lastPart]);
-	};
-	return (
-		<div className='parent'>
-			<h1>Parent Component</h1>
-			<span>
-				<label htmlFor='itemName'>Item Name</label>
-				<input
-					type='text'
-					id='itemName'
-					onChange={(e) => setItemName(e.target.value)}
-				/>
-			</span>
-			<span>
-				<label htmlFor='itemPrice'>Item Price</label>
-				<input
-					type='text'
-					id='itemPrice'
-					onChange={(e) => setItemPrice(e.target.value)}
-				/>
-			</span>
-			<button onClick={addItemHandler}>Add Item</button>
-			<br />
-			<h3>Child Component</h3>
-			<ul className='child'>
-				{itemList.map((item, index) => {
-					return (
-						<Child
-							idx={index}
-							item={item.itemName}
-							price={item.itemPrice}
-							removeItemHandler={removeItemHandler}
-						/>
-					);
-				})}
-			</ul>
-		</div>
-	);
-};
+    return (
+        <div className="parent">
+            <h1>Parent Component</h1>
+            <label>Item Name: </label>
+            <input type="text" value={itemName} id="itemName" onChange={(e) => setItemName(e.target.value)} />
+            <label>Item Price: </label>
+            <input type="number" id="itemPrice" value={itemPrice} onChange={(e) => setItemPrice(e.target.value)} />
+            <button onClick={handleClick} >Add Item</button>
+            <Child itemList={itemList} setItemList={setItemList}/>
+        </div>
+    )
+}
 
 export default Parent;
